e4da8f21a4cc18de6464790b593b1b87675c1b79
Cody Green <cody@codygreen.com>
2019-09-26 15:21:40 -0500

    abstracted ec2_key_name variable; cleanup

--- examples/main.tf
+++ examples/main.tf
@@ -2,12 +2,23 @@ provider "aws" {
   region = local.region
 }
 
+# Variable for the EC2 Key 
+# Set via CLI or via terraform.tfvars file
+variable "ec2_key_name" {
+  description = "AWS EC2 Key name for SSH access"
+  type        = string
+}
+
+#
 # Create a random id
+#
 resource "random_id" "id" {
   byte_length = 2
 }
 
+#
 # Create the VPC
+#
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -30,6 +41,9 @@ module "vpc" {
   }
 }
 
+#
+# Create a security group for port 80 traffic
+#
 module "web_server_sg" {
   source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
@@ -37,9 +51,12 @@ module "web_server_sg" {
   description = "Security group for web-server with HTTP ports"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks = [local.allowed_app_cidr]
+  ingress_cidr_blocks = [local.cidr, local.allowed_app_cidr]
 }
 
+#
+# Create a security group for port 80 traffic
+#
 module "ssh_secure_sg" {
   source = "terraform-aws-modules/security-group/aws//modules/ssh"
 
@@ -47,10 +64,12 @@ module "ssh_secure_sg" {
   description = "Security group for SSH ports open within VPC"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks = [local.allowed_mgmt_cidr]
+  ingress_cidr_blocks = [local.cidr, local.allowed_mgmt_cidr]
 }
 
+#
 # Create NGINX Instance
+#
 module aws-nginx-demo {
   source = "../"
 
@@ -60,7 +79,7 @@ module aws-nginx-demo {
     random_id.id.hex
   )
   associate_public_ip_address = true
-  ec2_key_name                = local.ec2_key_name
+  ec2_key_name                = var.ec2_key_name
   vpc_security_group_ids = [
     module.web_server_sg.this_security_group_id,
     module.ssh_secure_sg.this_security_group_id
@@ -76,6 +95,4 @@ locals {
   cidr              = "10.0.0.0/16"
   allowed_app_cidr  = "0.0.0.0/0"
   allowed_mgmt_cidr = "0.0.0.0/0"
-  ec2_key_name      = "cody-key"
-  private_key_path  = "~/.ssh/cody-key.pem"
 }
